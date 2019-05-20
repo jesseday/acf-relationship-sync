6cdab1e85480d321d9a5180e804bbfbce0794f2f
Jesse Day <09jday@gmail.com>
2019-05-20 17:10:44 -0600

    Rename plugin file with php extension

---
+++ acf-relationship-sync.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     ACF Relationship Sync
+ * Description:     Allow ACF relationship fields to be bidirectional, syncing configured fields on update.
+ * Author:          Jesse Day
+ * Author URI:      jesseday.us
+ * Text Domain:     acf-relationship-sync
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Acf_Relationship_Sync
+ */
+
+require(plugin_dir_path( __FILE__ ) . 'src/Types/OneToMany.php');
